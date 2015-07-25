c72b46909236e40a4ad1b11197d8020a502b8b7d
edgar bernal remoto <tecualoyaster@gmail.com>
2015-07-25 13:45:54 -0500

    HolaMundo Speal
    se realiza el hola mundo con Speal, para saber como crear y procesar
    expresiones

--- labs-modulo2/src/test/java/com/synergyj/cursos/spring/pruebas/HolaMundoSpELTestCase.java
+++ labs-modulo2/src/test/java/com/synergyj/cursos/spring/pruebas/HolaMundoSpELTestCase.java
@@ -13,6 +13,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 /**
  * @author Jorge Rodríguez Campos (jorge.rodriguez@synergyj.com)
@@ -22,22 +25,35 @@ public class HolaMundoSpELTestCase {
 	/**
 	 * Logger para todas las instancias de la clase
 	 */
-	private static final Logger logger = LoggerFactory.getLogger(HolaMundoSpELTestCase.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(HolaMundoSpELTestCase.class);
 
 	@Test
 	public void testSpel() throws Exception {
 		String message = null;
-		// TODO, evaluar una expresion con SpEL empleando los siguientes puntos para que la prueba
+		// TODO, evaluar una expresion con SpEL empleando los siguientes puntos
+		// para que la prueba
 		// sea exitosa.
 
-		// 1) Creae una instancia de ExpressionParser para procesar las expresiones
+		// 1) Creae una instancia de ExpressionParser para procesar las
+		// expresiones
 
-		// 2) Crear un objeto Expression lista para ser evaluada a partir de una cadena entre
+		ExpressionParser expressionParser = new SpelExpressionParser();
+
+		// 2) Crear un objeto Expression lista para ser evaluada a partir de una
+		// cadena entre
 		// comillas que será parseada.
 
-		// 3) evaluar la expresion y mostrar su resultado a través del método getValue()
+		Expression exp = expressionParser.parseExpression("'SynergyJ'");
 
-		// 4) Evaluar la expresión recuperando el resultado con el uso de Genericos.
+		// 3) evaluar la expresion y mostrar su resultado a través del método
+		// getValue()
+
+		message = (String) exp.getValue();
+		// 4) Evaluar la expresión recuperando el resultado con el uso de
+		// Genericos.
+
+		message = exp.getValue(String.class);
 
 		assertEquals("SynergyJ", message);
 
